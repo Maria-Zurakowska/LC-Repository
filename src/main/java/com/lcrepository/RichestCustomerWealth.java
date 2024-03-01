33a72e6c9a305349fa46d45faf20119114cfb448
Maria-Zurakowska <maria.zurakowska@gmail.com>
2024-03-01 18:08:59 +0100

    Add RichestCustomerWealth class which calculates clients' wealth using two-dimensional array.

---
+++ src/main/java/com/lcrepository/RichestCustomerWealth.java
@@ -0,0 +1,36 @@
+package com.lcrepository;
+
+public class RichestCustomerWealth {
+
+    public static void main(String[] args) {
+
+        int[][] accounts = {
+
+                {1, 5},
+                {7, 3},
+                {3, 5}
+        };
+
+        System.out.println(maximumWealth(accounts));
+    }
+
+    public static int maximumWealth(int[][] accounts) {
+
+        int maximumWealth = 0;
+
+        for (int i = 0; i < accounts.length; i++) {
+
+            int currentWealth = 0;
+
+            for (int j = 0; j < accounts[i].length; j++) {
+
+                currentWealth = currentWealth + accounts[i][j];
+            }
+
+            if (currentWealth > maximumWealth) {
+                maximumWealth = currentWealth;
+            }
+        }
+        return maximumWealth;
+    }
+}
