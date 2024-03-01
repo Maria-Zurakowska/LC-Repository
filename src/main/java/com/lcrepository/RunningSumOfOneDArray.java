96690d06d524e8eaa3a93653b363fc7eef4bf4ff
Maria-Zurakowska <maria.zurakowska@gmail.com>
2024-03-01 11:12:45 +0100

    Add Running sum of a one-dimentional array class along with the solution

---
+++ src/main/java/com/lcrepository/RunningSumOfOneDArray.java
@@ -0,0 +1,27 @@
+package com.lcrepository;
+
+import lombok.extern.slf4j.Slf4j;
+import java.util.Arrays;
+
+@Slf4j
+public class RunningSumOfOneDArray {
+
+    public static void main(String[] args) {
+
+        int[] nums = {1, 2, 3, 4};
+        log.info(Arrays.toString(runningSum(nums)));
+
+    }
+
+    public static int[] runningSum(int[] nums) {
+
+        int[] runningSum = new int[nums.length];
+        runningSum[0] = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+
+            runningSum[i] = runningSum[i - 1] + nums[i];
+        }
+        return runningSum;
+    }
+}
