101c71f09229cba7ba84f116e86cb0586ac698aa
Maria-Zurakowska <maria.zurakowska@gmail.com>
2024-03-01 10:33:49 +0100

    Add first commit

---
+++ src/main/java/com/lcrepository/LcRepositoryApplication.java
@@ -0,0 +1,13 @@
+package com.lcrepository;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LcRepositoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LcRepositoryApplication.class, args);
+    }
+
+}
