fc5dfc0b8fe7f9a40a7230f8d025b1711ecaee00
Maria-Zurakowska <maria.zurakowska@gmail.com>
2024-04-30 09:11:32 +0200

    Add test

--- src/main/java/com/codingchallenges/lc/HappyNumber.java
+++ src/main/java/com/codingchallenges/lc/HappyNumber.java
@@ -7,18 +7,9 @@ public class HappyNumber {
 
     public static void main(String[] args) {
 
-        int n = 123456;
-        List<Integer> list = new ArrayList<>();
+        int n = 19;
 
-        for (Integer i : list){
-            System.out.println(i);
-        }
-
-        numberToListOfDigits(n, list);
-        for(Integer nb : list){
-            System.out.println(nb);
-        }
-        System.out.println(digitSquareSum(list));
+        System.out.println(isHappy(n));
     }
 
     public static List<Integer> numberToListOfDigits(int n, List<Integer> numberDigits){
@@ -40,5 +31,26 @@ public class HappyNumber {
             sum = sum + digitsList.get(i) * digitsList.get(i);
         }
         return sum;
+    }
+
+    public static boolean isHappy(int n){
+
+            int temp = n;
+
+            if(temp < 10) { // Dlaczego < 10, chodzi o to, ze gdy liczba jest pojedyncza, konczymy rekursje gdy liczba == 1 i dla == 7 wynik koncowy w rekursji tez jest happy
+                if (temp == 1 || temp == 7) { return true; //dla reszty liczb tak nie jest, bez tego mialam problem infinite loop a potem stack overflow.
+                } else return false;
+            }
+
+        List<Integer> list = new ArrayList<>(); // pusta lista
+
+        List<Integer> digitsList = numberToListOfDigits(temp, list);// liczba zamieniona na liste cyfr z tej liczby {1,2,3,4,5,6}, List<Integer> list
+
+        int squareSum = digitSquareSum(digitsList);// suma kwadratow cyfr z liczby ||| {1,2,3,4,5,6} --> 1kw,2kw,3kw,4kw,5kw,6kw = 91
+
+        temp = squareSum;
+        digitsList.clear(); // czyszcze liste {1,2,3,4,5,6} --> teraz PUSTA
+//.test
+        return isHappy(temp);
     }
 }
